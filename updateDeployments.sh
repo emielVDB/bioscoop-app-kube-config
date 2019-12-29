e9ad32c94960b2a8764c0868f2fe1d1f0b860b38
emiel vandenbussche <emielPC@MacBook-Pro-van-emiel.local>
2019-12-29 19:48:18 +0100

    update script

--- updateDeployments.sh
+++ updateDeployments.sh
@@ -1,7 +1,7 @@
 git pull
-kubectl apply -f gateway-deployment.yml
-kubectl apply -f hall-deployment.yml
-kubectl apply -f media-deployment.yml
-kubectl apply -f schedule-deployment.yml
-kubectl apply -f ticket-deployment.yml
+kubectl apply -f ./gateway-deployment.yml
+kubectl apply -f ./hall-deployment.yml
+kubectl apply -f ./media-deployment.yml
+kubectl apply -f ./schedule-deployment.yml
+kubectl apply -f ./ticket-deployment.yml
 
