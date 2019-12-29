c0192e82c56d4831395f9ad11db50fce08f97495
emiel vandenbussche <emielPC@MacBook-Pro-van-emiel.local>
2019-12-29 20:10:39 +0100

    staff

--- updateDeployments.sh
+++ updateDeployments.sh
@@ -6,4 +6,10 @@ kubectl apply -f media-deployment.yaml
 kubectl apply -f schedule-deployment.yaml
 kubectl apply -f ticket-deployment.yaml
 
+kubectl apply -f staff-deployment.yaml
+kubectl apply -f catering-deployment.yaml
+kubectl apply -f statistics-deployment.yaml
+kubectl apply -f advertisement-deployment.yaml
+
+
 kubectl get rs
